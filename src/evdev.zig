92a25e1f9308fd44c7ff96acec5a5bfed8e3bc80
hylo <hylo@posteo.de>
2023-12-23 12:00:17 +0100

    feat: integrate touchpad & pen, refactor

--- src/evdev.zig
+++ src/evdev.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const l = @cImport({
+pub const l = @cImport({
     @cInclude("libevdev-1.0/libevdev/libevdev.h");
     @cInclude("libevdev-1.0/libevdev/libevdev-uinput.h");
 });
@@ -7,19 +7,22 @@ const l = @cImport({
 fn setup_abs(dev: ?*l.struct_libevdev, code: c_uint, min: c_int, max: c_int, resolution: ?c_int) void {
     const data_t = extern struct { value: c_int, minimum: c_int, maximum: c_int, fuzz: c_int, flat: c_int, resolution: c_int };
     var data = data_t{ .minimum = min, .maximum = max, .resolution = resolution orelse 0, .fuzz = 0, .flat = 0, .value = 0 };
-    var e = l.libevdev_enable_event_code(dev, l.EV_ABS, code, &data);
-    std.debug.print("{}", .{e});
+    _ = l.libevdev_enable_event_code(dev, l.EV_ABS, code, &data);
 }
 
-pub fn setup() ?*l.libevdev_uinput {
+pub fn setup_pen() ?*l.libevdev_uinput {
     var dev = l.libevdev_new();
-    if (dev == null) {
-        @panic("wtf");
-    }
-    // _ = l.libevdev_enable_event_type(dev, l.EV_ABS);
-    // _ = uidev;
 
-    l.libevdev_set_name(dev, "remvd");
+    l.libevdev_set_name(dev, "remarkable_pen");
+    // l.libevdev_set_id_bustype(dev, 0x03);
+    // l.libevdev_set_id_product(dev, 0x0361);
+    // l.libevdev_set_id_vendor(dev, 0x156a);
+    // l.libevdev_set_id_version(dev, 54);
+    // l.libevdev_set_id_bustype(dev, 0x6);
+    // l.libevdev_set_id_product(dev, 0x1701);
+    // l.libevdev_set_id_vendor(dev, 0x1701);
+    // l.libevdev_set_id_version(dev, 0x1);
+
     _ = l.libevdev_enable_event_type(dev, l.EV_REL);
     _ = l.libevdev_enable_event_code(dev, l.EV_REL, l.REL_X, null);
     _ = l.libevdev_enable_event_code(dev, l.EV_REL, l.REL_Y, null);
@@ -37,12 +40,44 @@ pub fn setup() ?*l.libevdev_uinput {
     _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_1, null);
     _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_2, null);
 
+    _ = l.libevdev_enable_event_type(dev, l.EV_ABS);
+    setup_abs(dev, l.ABS_X, 0, 20966, 100);
+    setup_abs(dev, l.ABS_Y, 0, 15725, 100);
+    setup_abs(dev, l.ABS_PRESSURE, 0, 4095, null);
+    setup_abs(dev, l.ABS_DISTANCE, 0, 255, null);
+    setup_abs(dev, l.ABS_TILT_X, -9000, 9000, 9000);
+    setup_abs(dev, l.ABS_TILT_Y, -9000, 9000, 9000);
+
+    var uidev: ?*l.libevdev_uinput = null;
+    _ = l.libevdev_uinput_create_from_device(dev, l.LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev);
+
+    return uidev;
+}
+
+pub fn setup_touchpad() ?*l.libevdev_uinput {
+    var dev = l.libevdev_new();
+
+    l.libevdev_set_name(dev, "remarkable_touchpad");
+    _ = l.libevdev_enable_property(dev, l.INPUT_PROP_POINTER);
+    l.libevdev_set_id_bustype(dev, 0x03);
+
+    // remarkable doesn't send these, but it seems to help with evdev/libinput
+    // recognizing this device as having pointer & gesture capabilities
+    _ = l.libevdev_enable_event_type(dev, l.EV_KEY);
+    _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_LEFT, null);
+    _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_MIDDLE, null);
+    _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_RIGHT, null);
+    _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_TOUCH, null);
+    _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_TOOL_FINGER, null);
+    _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_TOOL_DOUBLETAP, null);
+    _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_TOOL_TRIPLETAP, null);
+    _ = l.libevdev_enable_event_code(dev, l.EV_KEY, l.BTN_TOOL_QUADTAP, null);
+
     //     touch inputs
     _ = l.libevdev_enable_event_type(dev, l.EV_ABS);
-    // setup_abs(dev, l.ABS_MT_POSITION_X, 0, 20966, 100);
-    setup_abs(dev, l.ABS_MT_POSITION_X, 0, 20967, 100);
-    setup_abs(dev, l.ABS_MT_POSITION_Y, 0, 15725, 100);
-    setup_abs(dev, l.ABS_MT_PRESSURE, 0, 4095, null);
+    setup_abs(dev, l.ABS_MT_POSITION_X, 0, 1871, 10);
+    setup_abs(dev, l.ABS_MT_POSITION_Y, 0, 1403, 10);
+    setup_abs(dev, l.ABS_MT_PRESSURE, 0, 256, null);
     setup_abs(dev, l.ABS_MT_TOUCH_MAJOR, 0, 255, null);
     setup_abs(dev, l.ABS_MT_TOUCH_MINOR, 0, 255, null);
     setup_abs(dev, l.ABS_MT_ORIENTATION, -127, 127, null);
@@ -50,20 +85,10 @@ pub fn setup() ?*l.libevdev_uinput {
     setup_abs(dev, l.ABS_MT_TOOL_TYPE, 0, 1, null);
     setup_abs(dev, l.ABS_MT_TRACKING_ID, 0, 65535, null);
 
-    //     pen inputs
-    setup_abs(dev, l.ABS_X, 0, 20967, 100);
-    setup_abs(dev, l.ABS_Y, 0, 15725, 100);
-    setup_abs(dev, l.ABS_PRESSURE, 0, 4095, null);
-    setup_abs(dev, l.ABS_DISTANCE, 0, 255, null);
-    setup_abs(dev, l.ABS_TILT_X, -6400, 6400, 6400);
-    setup_abs(dev, l.ABS_TILT_Y, -6400, 6400, 6400);
-
     var uidev: ?*l.libevdev_uinput = null;
-    var e = l.libevdev_uinput_create_from_device(dev, l.LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev);
+    _ = l.libevdev_uinput_create_from_device(dev, l.LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev);
 
-    std.debug.print("{}\n", .{e});
     return uidev;
-    // defer l.libevdev_uinput_destroy(uidev);
 }
 
 pub fn write(uidev: ?*l.libevdev_uinput, etype: c_uint, code: c_uint, value: c_int) void {
--- src/main.zig
+++ src/main.zig
@@ -1,131 +1,104 @@
 const std = @import("std");
-// const c = @cImport({
-//     @cInclude("muinput.h");
-// });
-// const c = @import("uinput.zig");
 const evdev = @import("evdev.zig");
+const l = evdev.l;
 
 const ns_per_s: u64 = 1000 * 1000 * 1000;
 
 const Point = struct { x: i32, y: i32 };
 
-fn map_to_monitor(in_x: i32, in_y: i32) Point {
-    const w_screen: f32 = 1920.0 + 2560.0;
-    _ = w_screen;
-    const h_screen: i32 = 1440.0;
-    _ = h_screen;
-    const w_rem = 20966.0;
-    _ = w_rem;
-    const h_rem = 15725.0;
-    _ = h_rem;
-
+fn map_x_to_monitor(in_x: i32) i32 {
     const origin_x = 20966.0 * (1920.0 / (1920.0 + 2560.0));
+
+    const p_x: i32 = @intFromFloat(origin_x + @as(f32, @floatFromInt(in_x)) * ((20966.0 - origin_x) / 20966.0) / (20966.0 / 15725.0));
+
+    return p_x;
+}
+
+fn map_y_to_monitor(in_y: i32) i32 {
     const origin_y = 0.0;
 
     // these two span the whole monitor, but not the whole tablet
     // const p_x: i32 = @intFromFloat(origin_x + @as(f32, @floatFromInt(in_x)) * 2560.0 / (1920.0 + 2560.0));
     // const p_y: i32 = @intFromFloat((origin_y + @as(f32, @floatFromInt(in_y)) * 20966.0 / 15725.0) - (20966.0 - 15725.0) * 0.5);
 
-    const p_x: i32 = @intFromFloat(origin_x + @as(f32, @floatFromInt(in_x)) * ((20966.0 - origin_x) / 20966.0) / (20966.0 / 15725.0));
     const p_y: i32 = @intFromFloat((origin_y + @as(f32, @floatFromInt(in_y))));
 
-    return .{ .x = p_x, .y = p_y };
+    return p_y;
+}
+
+fn fail() void {
+    std.debug.print("Please provide either \"touchpad\" or \"pen\" as first argument.\n", .{});
+    std.os.exit(1);
 }
 
 pub fn main() anyerror!void {
-    const allocator = std.heap.page_allocator;
-    _ = allocator;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
-    // a();
-    // var fd: c_int = c.setup();
-    // defer c.destroy(fd);
+    const args = try std.process.argsAlloc(allocator);
+    if (args.len < 2) {
+        fail();
+    }
 
-    // const path = "/home/n/remarkable/event1";
-    // const file = try std.fs.openFileAbsolute(path, .{ .mode = .read_only });
-    // defer file.close();
+    var dev: ?*l.libevdev_uinput = null;
+    if (std.mem.eql(u8, args[1], "pen")) {
+        dev = evdev.setup_pen();
+    } else if (std.mem.eql(u8, args[1], "touchpad")) {
+        dev = evdev.setup_touchpad();
+    } else {
+        fail();
+    }
 
-    const dev = evdev.setup();
     defer evdev.destroy(dev);
 
     const stdin = std.io.getStdIn().reader();
 
     var buffer: [16]u8 = undefined;
     var bytesRead: usize = 16;
-    var x: i64 = 0;
-    _ = x;
-    var y: i64 = 0;
-    _ = y;
-    var x_rel: i64 = 0;
-    _ = x_rel;
-    var y_rel: i64 = 0;
-    _ = y_rel;
-
-    const every = 10;
-    _ = every;
-    var counter: i64 = 0;
-    _ = counter;
-
-    var pressed: bool = false;
-    _ = pressed;
 
     while (bytesRead == 16) {
         bytesRead = try stdin.read(buffer[0..]);
 
         if (bytesRead != 16 and bytesRead != 0) {
-            // Handle unexpected read size
             std.debug.print("Unexpected read size: {}\n", .{bytesRead});
             break;
         }
 
-        // if (counter < 4) {
-        //     counter += 1;
-        //     continue;
-        // }
-        // counter = 0;
-        // var myStruct = MyStruct{};
-        // _ = myStruct; u4 u4 u2 u2 i4
-        // e_time, e_millis, e_type, e_code, e_value = struct.unpack('2IHHi', data)
-        const time: u32 = std.mem.readIntLittle(u32, buffer[0..4]);
-        _ = time;
-        const millis: u32 = std.mem.readIntLittle(u32, buffer[4..8]);
-        _ = millis;
+        // const time: u32 = std.mem.readIntLittle(u32, buffer[0..4]);
+        // _ = time;
+        // const millis: u32 = std.mem.readIntLittle(u32, buffer[4..8]);
+        // _ = millis;
         const dtype: u16 = std.mem.readIntLittle(u16, buffer[8..10]);
         var code: u16 = std.mem.readIntLittle(u16, buffer[10..12]);
         var value: i32 = std.mem.readIntLittle(i32, buffer[12..16]);
         // std.debug.print("{}\t{}\t{}\t{}\t{}\n", .{ time, millis, dtype, code, value });
 
-        if (dtype == 3) {
-            // const n_val: i64 = @divTrunc(@as(i64, @intCast(value)), 10);
-            // const n_val = value / 10;
-            if (code == 0) {
-                // std.debug.print("x: {}\n", .{value});
-                // value = @divTrunc(value, 20);
-                value = map_to_monitor(value, 0).x;
-                // if (x != 0)
-                //     x_rel = n_val - x;
-                // x = n_val;
-                // // std.debug.print("{}\n", .{x});
-                // value = x;
-            } else if (code == 1) {
-                // std.debug.print("y: {}\n", .{value});
-                // value = @divTrunc(value, 20);
-                value = value;
-                value = map_to_monitor(value, value).y;
-                // value = value / 10;
-                // if (y != 0)
-                //     y_rel = n_val - y;
-                // y = n_val;
-                // value = y;
-            } else if (code == 26) {
-                code = 27;
-                value = -1 * value;
-            } else if (code == 27) {
-                code = 26;
-                // value = -1 * value;
+        if (dtype == l.EV_ABS) {
+            switch (code) {
+                l.ABS_X => {
+                    value = map_x_to_monitor(value);
+                },
+                l.ABS_Y => {
+                    value = map_x_to_monitor(value);
+                },
+                l.ABS_TILT_X => {
+                    code = l.ABS_TILT_Y;
+                    value = -1 * value;
+                },
+                l.ABS_TILT_Y => {
+                    code = l.ABS_TILT_X;
+                },
+                l.ABS_MT_POSITION_X => {
+                    code = l.ABS_MT_POSITION_Y;
+                },
+                l.ABS_MT_POSITION_Y => {
+                    code = l.ABS_MT_POSITION_X;
+                },
+                else => {},
             }
         }
 
-        // c.emit(fd, @as(c_int, @intCast(dtype)), @as(c_int, @intCast(code)), @as(c_int, @intCast(value)));
         evdev.write(dev, dtype, code, value);
     }
 }

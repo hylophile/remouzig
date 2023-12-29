dc54c44fe9d2b3e73718682223c3579dc2ae3b7b
hylo <hylo@posteo.de>
2023-12-30 00:33:27 +0100

    build: make nix build work

--- flake.nix
+++ flake.nix
@@ -4,20 +4,33 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    zig.url = "github:mitchellh/zig-overlay";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    gitignore.url = "github:hercules-ci/gitignore.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, zig }:
+  outputs = { self, nixpkgs, flake-utils, zig-overlay, gitignore }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages = { default = pkgs.libevdev; };
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        zig = zig-overlay.packages.${system}.master;
+      in rec {
         formatter = pkgs.nixpkgs-fmt;
 
+        packages.default = packages.zig-rem-input;
+        packages.zig-rem-input = pkgs.stdenv.mkDerivation {
+          name = "zig-rem-input";
+          version = "master";
+          src = gitignore.lib.gitignoreSource ./.;
+          nativeBuildInputs = [ zig pkgs.libevdev ];
+          buildPhase = ''
+            export NIX_CFLAGS_COMPILE=" -isystem ${pkgs.libevdev}/include/libevdev-1.0/ $NIX_CFLAGS_COMPILE";
+            export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig"
+            zig build -Doptimize=ReleaseFast --prefix $out
+          '';
+        };
+
         devShell = pkgs.mkShell {
-          buildInputs =
-            [ pkgs.libevdev pkgs.zls zig.packages.${system}.master ];
-          buildPhase = "zig build";
+          buildInputs = [ pkgs.libevdev pkgs.zls zig ];
           shellHook = ''
             export NIX_CFLAGS_COMPILE=" -isystem ${pkgs.libevdev}/include/libevdev-1.0/ $NIX_CFLAGS_COMPILE";
           '';
